4f8f82feefbf26dfd84d30cddfee49ac0622cc3e
sunrinjh <ysjh@yonsei.ac.kr>
2023-07-08 23:32:47 +0900

    finalized

--- main.py
+++ main.py
@@ -1,24 +1,123 @@
-import tkinter
+import tkinter as tk
+import tkinter.font
+import tarfile
+from tempfile import TemporaryDirectory
+import os
 from tkinter import filedialog
+from tkinter import simpledialog
 from tkinter import ttk
+from PIL import ImageTk, Image
 from pdf2img import findFromFile
 
-app = tkinter.Tk()
-filename:str = ""
-def select_file():
-    filetypes = (("PDF files", "*.pdf"), )
+
+app = tk.Tk()
+app.geometry("800x600")
+tabControl = ttk.Notebook(app)
+tabControl.pack(expand=True, fill="both")
+loaderTab = ttk.Frame(tabControl, width=800, height=300)
+viewerTab = ttk.Frame(tabControl, width=800, height=300)
+
+tabControl.add(loaderTab, text="Loader")
+tabControl.add(viewerTab, text="Viewer")
+
+
+filename: str = ""
+booknameInput = tk.StringVar()
+eqnnumInput = tk.StringVar()
+
+tempdir = TemporaryDirectory().name
+
+
+def select_file(booknameInput: str):
+    bookname = booknameInput.get()
+    if bookname == "":
+        return
+    while os.path.exists("./data/" + bookname):
+        bookname = simpledialog.askstring(
+            title="Re-Enter Your Bookname", prompt="Duplicated Bookname."
+        )
+
+    filetypes = (("PDF files", "*.pdf"),)
 
     filename = filedialog.askopenfilename(
         title="Open a file", initialdir="~", filetypes=filetypes
     )
     print(filename)
-    findFromFile(filename)
-    
-    
+    findFromFile(filepath=filename, bookname=bookname)
+    combo.config(values=os.listdir("./data"))
 
 
-open_button = ttk.Button(app, text="Open a File", command=select_file)
+loader_Label = ttk.Label(
+    loaderTab,
+    text="Write Down Your Book Name. (It will be the database's file name)\nIf you don't you can't select the file. sry\nLoading will take some time, it's good time to stand up from your desk and drink some water.",
+    font=tkinter.font.Font(family="맑은 고딕", size=12),
+)
+loader_Label.pack()
+
+name_entry = ttk.Entry(
+    loaderTab,
+    width=50,
+    textvariable=booknameInput,
+    font=tkinter.font.Font(family="맑은 고딕", size=12),
+)
+name_entry.pack(expand=True)
+
+open_button = ttk.Button(
+    loaderTab, text="Open a File", command=lambda: select_file(booknameInput)
+)
 open_button.pack(expand=True)
 
-app.mainloop()
+equationNumber_entry = ttk.Entry(
+    viewerTab,
+    width=30,
+    textvariable=eqnnumInput,
+    font=tkinter.font.Font(family="맑은 고딕", size=10),
+)
+equationNumber_entry.pack(expand=True)
 
+combo = ttk.Combobox(viewerTab, state="readonly", values=os.listdir("./data"))
+def comboboxSelected(eventObject):
+    print(tempdir)
+    selection = combo.get()
+    if not os.path.exists(tempdir + "/" + selection):
+        with tarfile.open("./data/" + selection, "r") as tar:
+            os.makedirs(tempdir + "/" + selection)
+            tar.extractall(path=(tempdir +"/" +selection))
+            
+
+combo.bind("<<ComboboxSelected>>", comboboxSelected)
+combo.pack()
+
+
+img = Image.open("DoNotDelete.png")
+img = img.resize(
+    (round(img.width / img.width * 800), round(img.height / img.width * 800))
+)
+eqnImage = ImageTk.PhotoImage(image=img)
+
+eqnImage_Canvas = tk.Canvas(viewerTab, width=800, height=500)
+
+imgArea = eqnImage_Canvas.create_image(400, 250, image=eqnImage)
+eqnImage_Canvas.image = img
+eqnImage_Canvas.pack(expand=True)
+
+
+
+def update_image(var, index, mode):
+    global current
+    selection = combo.get()
+
+    global imgArea, img, eqnImage, eqnImage_Canvas
+    if os.path.exists(tempdir + "/" + selection + "/" + eqnnumInput.get() + ".png"):
+        img = Image.open(tempdir + "/" + selection + "/" + eqnnumInput.get() + ".png")
+        img = img.resize(
+            (round(img.width / img.width * 800), round(img.height / img.width * 800))
+        )
+        eqnImage = ImageTk.PhotoImage(image=img)
+        eqnImage_Canvas.itemconfig(imgArea, image=eqnImage)
+        eqnImage_Canvas.image = img
+
+
+eqnnumInput.trace("w", update_image)
+
+app.mainloop()
--- pdf2img.py
+++ pdf2img.py
@@ -1,38 +1,72 @@
 import re
 import fitz
 import os
+import tarfile
+from io import BytesIO
 from typing import Final
 
 
 baseDpi: Final[int] = 72
 desiredDpi: Final[int] = 300
-dpiRate = desiredDpi / baseDpi
+dpiRate: Final[int] = desiredDpi / baseDpi
 
 yImageOffset = 10
 xLeftImageOffset = 40
 xRightImageOffset = 25
 
-def findParenthesis(page:fitz.Page):
-    lst: list = page.get_text(")", clip = (340, 0, page.bound().x1, page.bound().y1))
-    
+output: list = []
+
+
+def findParenthesis(page: fitz.Page):
     regex = re.compile("^[(]\d.\d+[a-z]?[)]$")
     words = page.get_text("words", sort=True)
     matches = [w for w in words if regex.search(w[4])]
     for match in matches:
-        irect:fitz.IRect = fitz.Rect(match[0], match[1], match[2], match[3]).irect 
-        #page.draw_rect(irect, width=1.5, color=(1, 0, 0))
+        irect: fitz.IRect = fitz.Rect(match[0], match[1], match[2], match[3]).irect
+        # page.draw_rect(irect, width=1.5, color=(1, 0, 0))
         pix = page.get_pixmap(dpi=desiredDpi)
-        irect = fitz.IRect(xLeftImageOffset * dpiRate, (irect.y0 - yImageOffset) * dpiRate, (irect.x1 - xRightImageOffset) * dpiRate, (irect.y1 + yImageOffset) * dpiRate)
+        irect = fitz.IRect(
+            xLeftImageOffset * dpiRate,
+            (irect.y0 - yImageOffset) * dpiRate,
+            (irect.x1 - xRightImageOffset) * dpiRate,
+            (irect.y1 + yImageOffset) * dpiRate,
+        )
         pixn = fitz.Pixmap(fitz.csGRAY, irect, 0)
         pixn.set_origin(irect.x0, irect.y0)
         pixn.copy(pix, irect)
-        pixn.save(f"./imgdata/{match[4][1:-1]}.png")
+        output.append((pixn.tobytes(), f"{match[4][1:-1]}"))
 
 
-
-def findFromFile(filepath:str):
+def findFromFile(filepath: str, bookname: str):
     doc = fitz.open(filepath)
-    if not os.path.exists("./imgdata"):
-        os.makedirs("imgdata")
+    if not os.path.exists("./data"):
+        os.makedirs("data")
     for i, page in enumerate(doc):
         findParenthesis(page)
+    directories: dict = {}
+    with tarfile.open("./data/" + bookname, "w") as tar:
+        for imgByte, name in output:
+            name: str = name
+            for index, text in enumerate(name):
+                if not text.isalpha() and not text.isdecimal():
+                    name = name.replace(text, '.')                 
+            
+            # indexOfSemiColon = name.find(".")
+            # front = name[:indexOfSemiColon]
+            # back = name[indexOfSemiColon + 1 :]
+            # if front not in directories:
+            #     directories[front] = 1
+            #     dirInfo = tarfile.TarInfo(name=front)
+            #     dirInfo.type = tarfile.DIRTYPE
+            #     dirInfo.mode = 0o777
+            #     tar.addfile(tarinfo=dirInfo)
+
+            s = BytesIO()
+            s.write(imgByte)
+            s.seek(0)
+
+            tinfo = tarfile.TarInfo(name=(name+".png"))
+            tinfo.size = len(s.getbuffer())
+
+            tar.addfile(tarinfo=tinfo, fileobj=s)
+    tar.close()
